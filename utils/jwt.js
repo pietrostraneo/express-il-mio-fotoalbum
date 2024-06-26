1b3a1b9dc5ab100ac59928df1a0a07cb14c31ada
pietrostraneo <straneo_p@yahoo.it>
2024-06-26 12:54:34 +0200

    Utils file has been commented

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,3 +1,15 @@
+/**
+ * Generates a JSON Web Token (JWT) from a given payload.
+ *
+ * @param {Object} payload - The payload to be encoded in the JWT.
+ * @param {string} [expiresIn='8h'] - The time until the token expires. Can be a string in the format of 'Xm' (minutes), 'Xh' (hours), or 'Xd' (days).
+ * @returns {string} The generated JWT.
+ *
+ * @example
+ * const token = generateToken({ userId: 1, username: 'johnDoe' });
+ * console.log(token); // Output: a JSON Web Token string
+ */
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
--- utils/pass.js
+++ utils/pass.js
@@ -1,11 +1,13 @@
 const bcrypt = require('bcrypt');
 
+// Function that takes the password as an input parameter and returns a hashed password
 async function hashPassword(password) {
     const saltRounds = 10;
     const hash = await bcrypt.hash(password + process.env.HASH_KEY, saltRounds);
     return hash;
 }
 
+// Function that checks the correspondence between the password and the hashed password
 async function readPass(password, hashedPass) {
     const isValid = await bcrypt.compare(password + process.env.HASH_KEY, hashedPass)
     return isValid;
