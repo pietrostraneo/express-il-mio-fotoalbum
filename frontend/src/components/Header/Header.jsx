92cba08e3ccec183005d402efda5b92eb128b35c
pietrostraneo <straneo_p@yahoo.it>
2024-06-27 15:53:10 +0200

    Added favicon and link to fontawesome cdn

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,15 +4,19 @@ export default function Header() {
     return (
         <>
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12 d-flex justify-content-between">
+                <div className="row p-3 align-items-center">
+                    <div className="col-12 d-flex justify-content-between align-items-center">
 
                         <div className="logo">
-                            <img src="https://github.com/pietrostraneo/express-il-mio-fotoalbum/blob/main/frontend/src/assets/logo.png" alt="logo" className='img-fluid' />
+                            <img src="/logo.png" alt="logo" className={`img-fluid ${styleHeader.logo_img}`} />
                         </div>
 
                         <div className="nav">
-                            <h1 className={`${styleHeader.test}`}>TEST</h1>
+                            <ul className="d-flex list-unstyled">
+                                <li className="nav-item"></li>
+                                <li className="nav-item"></li>
+                                <li className="nav-item"></li>
+                            </ul>
                         </div>
 
                     </div>
